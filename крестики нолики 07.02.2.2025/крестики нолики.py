5670e7523daaa007aa5108d8b3f76d0cd72d0c58
марк1511 <146611926+mark01298@users.noreply.github.com>
2025-02-07 20:14:29 +0700

    Add files via upload

---
+++ крестики нолики 07.02.2.2025/крестики нолики.py
@@ -0,0 +1,53 @@
+import pyautogui as p
+import time
+p.hotkey("win" , "r")
+p.write("mspaint")
+p.press("enter")
+time.sleep(2)
+p.moveTo(300,400)
+
+
+p.dragTo(400,400, duration = 1)
+p.dragTo(400,300, duration = 1)
+p.dragTo(300,300, duration = 1)
+p.dragTo(300,400, duration = 1)
+p.moveTo(400,400)
+p.dragTo(500,400, duration = 1)
+p.dragTo(500,300, duration = 1)
+p.dragTo(400,300, duration = 1)
+p.dragTo(400,400, duration = 1)
+p.moveTo(500,400)
+p.dragTo(600,400, duration = 1)
+p.dragTo(600,300, duration = 1)
+p.dragTo(500,300, duration = 1)
+p.dragTo(500,400, duration = 1)
+p.moveTo(300,500)
+p.dragTo(600,500, duration = 1)
+p.dragTo(600,400, duration = 1)
+p.dragTo(500,400, duration = 1)
+p.dragTo(500,500, duration = 1)
+p.dragTo(400,500, duration = 1)
+p.dragTo(400,400, duration = 1)
+p.dragTo(300,400, duration = 1)
+p.dragTo(300,500, duration = 1)
+p.moveTo(300,600)
+p.dragTo(600,600, duration = 1)
+p.dragTo(600,500, duration = 1)
+p.dragTo(500,500, duration = 1)
+p.dragTo(500,600, duration = 1)
+
+p.dragTo(400,600, duration = 1)
+p.dragTo(400,500, duration = 1)
+p.dragTo(300,500, duration = 1)
+p.dragTo(300,600, duration = 1)
+
+
+p.dragTo(400,500, duration = 1)
+p.dragTo(300,500, duration = 1)
+p.dragTo(400,600, duration = 1)
+p.moveTo(400,500, duration =1)
+
+
+
+
+
