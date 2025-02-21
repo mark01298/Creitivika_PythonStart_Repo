5970243b56952226b28ae4d4a7c93a745a93e947
марк1511 <146611926+mark01298@users.noreply.github.com>
2025-02-21 20:08:56 +0700

    Add files via upload

---
+++ index md/aaaaaaaa.py
@@ -0,0 +1,14 @@
+import pyautogui as p
+import time
+p.hotkey("win","r")
+p.write("mspaint")
+p.press("enter")
+p.moveTo(200,200)
+p.mouseDown()
+p.moveTo(300,200)
+p.moveTo(300,300)
+p.moveTo(200,300)
+p.moveTo(200,210)
+p.moveTo(290,210)
+p.moveTo(290,290)
+
